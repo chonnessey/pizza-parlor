3fd3eb04103bd11a10c28d434b13ad6c1c39ada4
Adrian Camacho <adriancamacho18@gmail.com>
2021-04-09 10:53:06 -0700

    add extra topping options

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,18 @@
 // Business logic
 
-function Pizza(topping, size, price) {
-  this.pizzaTopping = topping;
+function Pizza(toppings, size, price) {
+  this.pizzaToppings = toppings;
   this.pizzaSize = size;
   this.price = price;
 }
-console.log(priceOfPizza);
-Pizza.prototype.GetPrice = function() {
+
+Pizza.prototype.GetSize = function() {
   let sizeOfPizza = this.pizzaSize;
   let priceOfPizza = 0;
   this.price = 0;
-  switch(sizeOfPizza) {
-    case "small":
-      priceOfPizza = 10;
-      break;
-  }
-  return this.price += priceOfPizza;
 }
-console.log(priceOfPizza);
+
+Pizza.prototype.GetTopping
 
 
 
