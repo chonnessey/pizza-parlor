91fed306069413e69cec6abb67789248887ce156
Adrian Camacho <adriancamacho18@gmail.com>
2021-04-09 12:02:08 -0700

    WIP: testing different methods to solve this problem

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,23 @@
 // Business logic
 
-function Pizza(toppings, size, price) {
+function Pizza(toppings, size,) {
   this.pizzaToppings = toppings;
   this.pizzaSize = size;
-  this.price = price;
 }
 
-Pizza.prototype.GetSize = function() {
+Pizza.prototype.GetPrice = function() {
   let sizeOfPizza = this.pizzaSize;
+  let chosenToppings = [];
   let priceOfPizza = 0;
-  this.price = 0;
+  this.pizzaToppings.forEach(function(topping) {
+    chosenToppings.push(topping)
+  });
+  console.log(Pizza.GetPrice.chosenToppings);
+  // determine price of pizza depending on user input for toppings and size.
 }
 
-Pizza.prototype.GetTopping
+
+
 
 
 
@@ -26,8 +31,11 @@ Pizza.prototype.GetTopping
 $(document).ready(function() {
   $("#order").submit(function(event) {
     event.preventDefault();
-    const topping = $("#topping").val();
-    const size = $("#size").val();
+    const topping1 = $("#topping1").val();
+    const topping2 = $("#topping2").val();
+    const topping3 = $("#topping3").val();
+    const sizeOfPizza = $("#size").val();
     const priceOfPizza = 0;
+    let chosenToppings = [];
   });
 });
