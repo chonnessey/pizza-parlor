cbba3fbd957b12b5d4032271f8ea18d1ee91f143
Adrian Camacho <adriancamacho18@gmail.com>
2021-04-22 19:52:46 -0700

    add the rest of functionality to scripts

--- js/scripts.js
+++ js/scripts.js
@@ -7,30 +7,42 @@ function Pizza(topping1, topping2, topping3, size) {
   this.pizzaSize = size;
   this.price = 0;
 }
-console.log("small", this.price)
-
 
 Pizza.prototype.getPrice = function() {
   if (this.pizzaSize === "small") {
     this.price += 15;
   } else if (this.pizzaSize === "medium") {
     this.price += 20;
+  } else if (this.pizzaSize === "large") {
+    this.price += 25;
+  }
+  if (this.topping1 === "topping1") {
+    this.price += 2;
+  } else {
+    this.price += 0;
+  }
+  if (this.topping2 === "topping2") {
+    this.price += 2;
+  } else {
+    this.price += 0;
+  }
+  if (this.topping3 === "topping3") {
+    this.price += 2;
+  } else {
+    this.price += 0;
   }
   return this.price;
 }
-console.log("Large", this.price)
-
-
 
 // UI logic 
 $(document).ready(function() {
   $("#order").submit(function(event) {
     event.preventDefault();
-    const topping1 = parseInt($("#topping1").val());
-    const topping2 = parseInt($("#topping2").val());
-    const topping3 = parseInt($("#topping3").val());
-    const mySize = parseInt($("#size").val());
-    let myPizza = new Pizza(mySize, topping1, topping2, topping3);
+    const topping1 = $("#topping1").val();
+    const topping2 = $("#topping2").val();
+    const topping3 = $("#topping3").val();
+    const mySize = $("#size").val();
+    let myPizza = new Pizza(topping1, topping2, topping3, mySize);
     myPizza.getPrice();
     $("#results").text(myPizza.price);
   });
